858cc8d6d295389ac3f49585cace61a660179eeb
Arbesa Kajtazi <arbesakajtazi@gmail.com>
2026-05-21 19:32:22 +0200

    remove debounce;

--- client/packages/big-vscode-contribution/src/env/vscode/dirty-state-handler.ts
+++ client/packages/big-vscode-contribution/src/env/vscode/dirty-state-handler.ts
@@ -17,8 +17,6 @@ import { TYPES } from '../common/types.js';
 import type { ActionDispatcher } from './action-dispatcher.js';
 import type { DocumentManager } from './document-manager.js';
 
-const EDIT_DEBOUNCE_MS = 300;
-
 @injectable()
 export class DirtyStateHandler<TDocument extends vscode.CustomDocument = vscode.CustomDocument>
     implements VscodeActionHandler<TDocument>
@@ -26,9 +24,6 @@ export class DirtyStateHandler<TDocument extends vscode.CustomDocument = vscode.
     readonly actionKinds = [SetDirtyStateAction.KIND] as const;
 
     protected readonly initializedClients = new Set<string>();
-    protected pendingEditTimer: ReturnType<typeof setTimeout> | undefined;
-    protected pendingEditClientId: string | undefined;
-    protected pendingEditDocument: TDocument | undefined;
 
     constructor(
         @inject(TYPES.DocumentManager) protected readonly documentManager: DocumentManager<TDocument>,
@@ -51,12 +46,19 @@ export class DirtyStateHandler<TDocument extends vscode.CustomDocument = vscode.
         const reason = action.reason;
 
         if (reason === 'save') {
-            this.flushPendingEdit();
             this.initializedClients.add(client.clientId);
             this.documentManager.notifyDocumentSaved(client.clientId, client.document);
         } else if (reason === 'operation' && action.isDirty) {
             if (this.initializedClients.has(client.clientId)) {
-                this.schedulePendingEdit(client);
+                this.documentManager.notifyDocumentEdit({
+                    document: client.document,
+                    undo: () => {
+                        this.actionDispatcher.dispatch(UndoAction.create(), client.clientId);
+                    },
+                    redo: () => {
+                        this.actionDispatcher.dispatch(RedoAction.create(), client.clientId);
+                    }
+                });
             }
         } else if (!action.isDirty) {
             this.initializedClients.add(client.clientId);
@@ -69,42 +71,5 @@ export class DirtyStateHandler<TDocument extends vscode.CustomDocument = vscode.
             processedMessage: message,
             messageChanged: true
         };
-    }
-
-    protected schedulePendingEdit(client: GlspVscodeClient<TDocument>): void {
-        if (this.pendingEditTimer !== undefined) {
-            clearTimeout(this.pendingEditTimer);
-        }
-
-        this.pendingEditClientId = client.clientId;
-        this.pendingEditDocument = client.document;
-
-        this.pendingEditTimer = setTimeout(() => {
-            this.flushPendingEdit();
-        }, EDIT_DEBOUNCE_MS);
-    }
-
-    protected flushPendingEdit(): void {
-        if (this.pendingEditTimer === undefined) {
-            return;
-        }
-
-        clearTimeout(this.pendingEditTimer);
-        this.pendingEditTimer = undefined;
-
-        const clientId = this.pendingEditClientId!;
-        const document = this.pendingEditDocument!;
-        this.pendingEditClientId = undefined;
-        this.pendingEditDocument = undefined;
-
-        this.documentManager.notifyDocumentEdit({
-            document,
-            undo: () => {
-                this.actionDispatcher.dispatch(UndoAction.create(), clientId);
-            },
-            redo: () => {
-                this.actionDispatcher.dispatch(RedoAction.create(), clientId);
-            }
-        });
     }
 }
